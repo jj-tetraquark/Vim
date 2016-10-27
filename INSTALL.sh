5917dc3447dd515fae403d9b35829d02cbafadd2
Jonathan Wilkinson <jonathan.wilkinson@dyson.com>
2016-10-27 09:04:45 +0100

    Installer adds powerline fonts

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,4 +6,10 @@ echo Make tmp dir for undofiles and such
 mkdir ~/.vimtmp
 echo Installing packages...
 vim -c "BundleInstall!"
+echo setting up fonts...
+git clone https://github.com/powerline/fonts.git
+cd fonts
+chmod +x install.sh
+bash install.sh
+
 echo Done
